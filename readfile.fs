2ae462b4d9cf0af2a35c3057b095e4e140918e0e
Dan Higdon <hdan@alumni.utexas.net>
2023-11-30 21:00:37 -0600

    Further refinements of the basic file read helper words

--- readfile.fs
+++ readfile.fs
@@ -3,25 +3,33 @@
 marker readfile
 
 variable read-file-handle
+256 constant input-buffer-len
+create input-buffer input-buffer-len allot
 
 : close-read ( -- )
-   read-file-handle @ ?dup if
-      close-file throw
-      0 read-file-handle !
-   then ;
+   read-file-handle @ ?dup if close-file throw
+   0 read-file-handle !  then ;
 
 \ Opens a file for reading. File must exist
 : open-read: ( "name" -- )
    32 parse r/o open-file abort" File not found"
    close-read  read-file-handle ! ;
 
-\ Read the next line into PAD.
+\ Read the next line into input-buffer
+\ Returns the string read or throws an error
 : line-read ( -- addr n flag )
-   pad dup 80 read-file-handle @ read-line throw ;
+   input-buffer dup input-buffer-len 
+   read-file-handle @ read-line throw ;
+
+\ Call xt on each line of the currently open file
+\ xt has the signature ( addr n -- )
+: process ( xt -- )
+   >r begin line-read while
+      r@ catch abort" Error reading file"
+   repeat 2drop r> drop ;
 
 \ A test and example of how to use these words
-: cat:
-   open-read:
-   begin line-read while type cr repeat 2drop
-   close-read ;
+: type-cr ( addr n -- ) type cr ;
+: cat: ( name -- )
+   open-read: ['] type-cr process close-read ;
 
